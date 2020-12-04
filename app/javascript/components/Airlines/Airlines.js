35028eb067bbec4ba916a23af2f44cdb676581cd
adclleva <arvin.lleva@yahoo.com>
2020-12-04 00:15:04 -0800

    Got the data from our api with axios

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
 
 function Airlines() {
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/v1/airlines.json")
+      .then((resp) => {
+        setAirlines(resp.data.data);
+      })
+      .catch((resp) => console.log(resp));
+  }, [airlines.length]);
+
+  const list = airlines.map((item) => {
+    return <li key={item.attributes.name}>{item.attributes.name}</li>;
+  });
+
   return (
-    <div>
-      This is the Airlines#index view for our app.
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest, unbiased airline reviews.</div>
+      </div>
+      <ul>{list}</ul>
     </div>
-  )
+  );
 }
 
-export default Airlines
+export default Airlines;
