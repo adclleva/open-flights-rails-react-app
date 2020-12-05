56ca3472e339eaa595a844ec54d97b1ad70253b3
adclleva <arvin.lleva@yahoo.com>
2020-12-05 10:37:51 -0800

    Created the Airline page and the Header component that comes with it

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./Header";
+import styled from "styled-components";
 
-function Airlines() {
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: black;
+  }
+`;
+
+const Main = styled.div`
+  padding-left: 50px;
+`;
+
+function Airlines(props) {
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    const url = `/api/v1/airlines/${slug}`;
+
+    axios
+      .get(url)
+      .then((resp) => {
+        setAirline(resp.data);
+        setLoaded(true);
+      })
+      .catch((resp) => console.log(resp));
+  }, []);
+
   return (
-    <div>
-      This is the Airlines#show view for our app.
-    </div>
-  )
+    <Wrapper>
+      <Column>
+        <Main>
+          {loaded && (
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          )}
+        </Main>
+        <div className="review"></div>
+      </Column>
+      <Column>
+        <div className="review-form">{"Review Form Goes Here."}</div>
+      </Column>
+    </Wrapper>
+  );
 }
 
-export default Airlines
+export default Airlines;
