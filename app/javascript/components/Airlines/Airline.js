82b082feabcfcedae2428fc2b315d8541b57e229
adclleva <arvin.lleva@yahoo.com>
2020-12-05 09:19:34 -0800

    Added the styled components, used css grid to style our airline grid.

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,19 +1,60 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+  text-align: center;
+`;
+
+const AirlineLogo = styled.div`
+  height: 50px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 10px;
+
+  img {
+    height: 50px;
+    width: 50px;
+    border: 1px solid #efefef;
+    border-radius: 100%;
+  }
+`;
+
+const AirlineName = styled.div`
+  padding: 20px 0 10px 0;
+`;
+
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height: 50px;
+
+  a {
+    color: #fff;
+    background-color: #000;
+    border-radius: 4px;
+    padding: 10px 50px;
+    border: 1px solid #000;
+    width: 100%;
+    text-decoration: none;
+  }
+`;
 
 function Airline(props) {
   const { attributes } = props;
   return (
-    <div className="card">
-      <div className="airline-logo">
+    <Card>
+      <AirlineLogo>
         <img src={attributes.image_url} alt={attributes.name} />
-      </div>
-      <div className="airline-name">{attributes.name}</div>
-      <div className="airline-score">{attributes.avg_score}</div>
-      <div className="airline-link">
+      </AirlineLogo>
+      <AirlineName>{attributes.name}</AirlineName>
+      <div>{attributes.avg_score}</div>
+      <LinkWrapper>
         <Link to={`/airlines/${attributes.slug}`}>View Airlines</Link>
-      </div>
-    </div>
+      </LinkWrapper>
+    </Card>
   );
 }
 
